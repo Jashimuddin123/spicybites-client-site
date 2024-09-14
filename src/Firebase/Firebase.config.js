622394a7fca394c17c74d061171a767e401669c0
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-09-14 22:07:48 +0600

    open a project using firebase

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB49Bj50SerPm7BAAT53rDJo0MeRhmXx_w",
+  authDomain: "spicybites-management.firebaseapp.com",
+  projectId: "spicybites-management",
+  storageBucket: "spicybites-management.appspot.com",
+  messagingSenderId: "1023410902860",
+  appId: "1:1023410902860:web:e3071c4aaf96fbd95161e9",
+  measurementId: "G-69VM5PXK0D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export default auth
