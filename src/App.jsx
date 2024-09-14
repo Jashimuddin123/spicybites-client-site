4ef8dc034d59315ba68bbd18adeb67a43831d3a1
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-09-14 11:16:23 +0600

    set up restaurent client site

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      
+       
+      </div>
+      <h1 className='text-5xl text-red-600'>Vite + React</h1>
+      <div className="card">
+    
+      </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div>
+            <h1>this is root file</h1>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
